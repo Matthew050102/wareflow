387246b27927b0e98f88dccf033ebf744a8f1e57
Matthew050102 <matthew0512@proton.me>
2026-08-12 13:23:47 +0200

    Creating project, initializing git

---
+++ src/main/java/com/matthew050102/wareflow/WareflowApplication.java
@@ -0,0 +1,13 @@
+package com.matthew050102.wareflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matthew050102/wareflow/WareflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew050102.wareflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
